3a962adb6079b3dfd2068fe55bb5251f0dd806f6
Carl J du Preez <carljdp@gmail.com>
2024-03-26 20:12:29 +0200

    logging: fix function to get context name

--- src/utils/Meta.ts
+++ src/utils/Meta.ts
@@ -1,34 +1,70 @@
 // file: /src/utils/Meta.ts
 
 
+function splitStackTraceLine(line: string): string[] {
+    // to split a string like "Object.fnName (path/to/file)"
+    // into ["async w.kb", "path/to/file"]
+
+    // remove the leading spaces and the 'at' keyword
+    const lean = line.replace(/^\s*at /, "").trim();
+
+    const part0noTrim = ((lean.match(/^([a-zA-z._$ ]+)\s+/) || [])[0] || "");
+    let part0 = part0noTrim.trim();
+    let part1 = lean.substring(part0noTrim.length).replace(/(^\()|(\)$)/g, "");
+
+    part0 = (part0.length > 0 ? part0 : "<anonymous>");
+    part1 = (part1.length > 0 ? part1 : "<unknown:0:0>");
+
+    return [ part0, part1 ];
+  }
+
+
 
 export function getContextName(context?: Function|null): string {
+    // 'caller' property is non-standard and cannot be used in strict mode.
+    // Hence, we will need to change the approach to pass the actual function or use Error stack trace.
+    
+    let callerName: string = "anonymous";
+    let callerType: string = "unknown";
 
-	// Types could be one of the following:
-	// "get "
-	// "set "
-	// "bound "
-	// "function "
-	// "class "
-	// "async "
-	// not handling "generator " or "async generator " for now
+    if (context) {
+        // Directly using the passed context's name property if available.
+        callerName = context.name || "anonymous";
 
-	const maybeName: string = (context === null) ? arguments.callee.caller.name : context?.name || context?.constructor?.name || context?.toString() || "";
-	let contextName: string = "anonymous";
-	let contextType: string = "anonymous";
-	
-	if (!((maybeName === undefined) || (maybeName === null) || (maybeName === ""))) {
+        // Checking if the context is an instance of a class.
+        if (context.prototype) {
+            callerType = "class";
+        } else {
+            callerType = "function";
+        }
+    } else {
+        // Using Error stack trace to get the calling function's name.
+        // because `caller` and `callee` is discouraged in strict mode.
 
-		const name = String(maybeName || "");
-		const space = String(name || "").indexOf(" ");
+        const stackLine = new Error().stack?.split('\n')[2].trim() || "";
 
-		if (space > 0) {
-			contextType = name.substring(0, space);
-			contextName = name.substring(space + 1);
-		}
+        // return a default value if the stackLine is empty.
+        const errorStackTrace = new Error().stack?.split('\n') || [
+            "Error: Error message",
+            "\tat anonymous (unknown-file:0:0)",
+            "\tat anonymous (unknown-file:0:0)",
+            "\tat anonymous (unknown-file:0:0)",
+        ];
 
-	}
+        // skipping the first line which is the error message.
+        const parsedStackTrace = errorStackTrace.slice(1).map(splitStackTraceLine);
 
-	return `${contextType}::${contextName}`;
+        const callerNameParts = parsedStackTrace[1][0].split(".");
+
+        if (callerNameParts.length === 2) {
+            callerName = callerNameParts[1];
+            callerType = callerNameParts[0];
+        } else {
+            callerName = callerNameParts[0];
+        }
+
+    }
+
+    return `${callerName}`;
 }
 
