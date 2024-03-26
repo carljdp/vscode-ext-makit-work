4b496515e1bd2463dc0642b709002f5ff305876d
Carl J du Preez <carljdp@gmail.com>
2024-03-26 20:17:41 +0200

    important vscode dirs

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+
+
+
+const USER_DATA_DIR_WINDO: Readonly<string> = "%APPDATA%\\Code";
+const USER_DATA_DIR_LINUX: Readonly<string> = "$HOME/.config/Code";
+const USER_DATA_DIR_MACOS: Readonly<string> = "$HOME/Library/Application Support/Code";
+
+const EXTENSIONS_DIR_WINDO: Readonly<string> = "%USERPROFILE%\\.vscode\\extensions";
+const EXTENSIONS_DIR_LINUX: Readonly<string> = "~/.vscode/extensions";
+const EXTENSIONS_DIR_MACOS: Readonly<string> = "~/.vscode/extensions";
