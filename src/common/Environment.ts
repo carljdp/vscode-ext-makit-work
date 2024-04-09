7ed8f253d0e2f485918fc7a5675d20e5a97cabaa
Carl J du Preez <carljdp@gmail.com>
2024-04-09 16:33:48 +0200

    adhoc settings etc

--- src/common/Environment.ts
+++ src/common/Environment.ts
@@ -1,8 +1,5 @@
 // file src/common/Environment.ts
 
-// TESTING:
-process.env.DEBUG = 'true';
-
 const _isDebug_ = ((env: NodeJS.ProcessEnv): boolean => {
     const value: string = JSON.stringify(env.DEBUG || '').replace(/['"]+/g, '').trim();
     return ['true', 'yes', 'on', '1'].includes(value.toLowerCase());
