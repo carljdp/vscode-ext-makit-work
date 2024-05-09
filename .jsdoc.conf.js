0ffef11ac25e94e7874e1a6a83b93238811f2863
Carl J du Preez <carljdp@gmail.com>
2024-05-09 17:57:15 +0200

    add todo/status/reminder

--- .jsdoc.conf.js
+++ .jsdoc.conf.js
@@ -1,5 +1,11 @@
 // file: .jsdoc.conf.js
 
+// TODOS:
+// - I can't exactly remember where we we're heading when we started this file
+// - I think we were trying to setup real-time jsdoc previews in vscode
+// - But were never able to quite get there
+
+
 'use strict';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
