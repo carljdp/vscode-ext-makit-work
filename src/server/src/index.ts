e82372bbeef93b806bec3b97fd3e370f264d7f45
Carl J du Preez <carljdp@gmail.com>
2024-05-12 17:51:46 +0200

    wip: src/server migrate build& debug launch to swc

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,20 +1,25 @@
-import path from 'node:path';
+
 import express from 'express';
 
 import app from './server.js';
+
+
 
 const _isDev_ = true;
 const _isDebug_ = true;
 
 import { fileURLToPath } from 'node:url';
+import { dirname, resolve, join } from 'node:path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Define the directory where your build artifacts are located
-const staticDir = path.resolve(fileURLToPath('./'), 'dist');
+const staticDir = resolve(join(__dirname, 'dist'));
 
 // Serve static files from the 'dist' directory
 app.use(express.static(staticDir));
 
 // Serve index.html for all routes to enable client-side routing
 app.get('*', (req, res) => {
-    res.sendFile(path.join(staticDir, 'index.html'));
+    res.sendFile(join(staticDir, 'index.html'));
 });
