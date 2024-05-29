3170f63c587672025094dc5ebbedda54e8ead0e2
Carl J du Preez <carljdp@gmail.com>
2024-05-29 20:13:37 +0200

    mostly cleanup. minor fixes.

---
+++ packages/extens/eslint.config.mjs
@@ -0,0 +1,95 @@
+"use strict";
+// @ts-check
+
+
+import { inspect } from 'node:util';
+import { getLogTag } from '../../packages/startx/devt/common/locations.js';
+const logTag = getLogTag();
+
+
+// CONSTANTS
+
+
+/** Manual debug flag for this script.
+ * @constant {boolean} DEBUG_THIS */
+const DEBUG_THIS = true;
+
+/** Whether to hit a breakpoint at the end of the script.
+ * @constant {boolean} DEBUG_PAUSE */
+const DEBUG_PAUSE = true;
+
+/** Whether to log out the configurations in detail.
+ * These verbose logs are not nested inside `DEBUG` blocks, as it can be useful even when `DEBUG` is false.
+ * @constant {boolean} LOG_VERBOSE */
+const LOG_VERBOSE = true;
+
+
+// IMPLEMENTATION
+
+
+if (process.env.DEBUG && DEBUG_THIS) console.log(`╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ${logTag} ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╮`);
+
+
+import { makeConfig, ResourceLocation, SourceType, LangType } from "../../eslint.config.mjs";
+
+
+const theUsualSuspects = [
+    'node_modules',
+    '.history',
+    '.cache',
+
+    // but for this vs code extension building workspace, also:
+    '.debug',
+    '.vscode-test',
+    'test',
+    'out',
+];
+
+const configs = [
+    // src / <esm|cjs> / ts
+    makeConfig(ResourceLocation.src.id, SourceType.module.id, LangType.ts.id, ['src'], ['dist', ...theUsualSuspects], false),
+    makeConfig(ResourceLocation.src.id, SourceType.commonjs.id, LangType.ts.id, ['src'], ['dist', ...theUsualSuspects], false),
+
+    // src / <esm|cjs> / js
+    makeConfig(ResourceLocation.src.id, SourceType.module.id, LangType.js.id, ['src'], ['dist', ...theUsualSuspects], false),
+    makeConfig(ResourceLocation.src.id, SourceType.commonjs.id, LangType.js.id, ['src'], ['dist', ...theUsualSuspects], false),
+
+    // dist / <esm|cjs> / js
+    makeConfig(ResourceLocation.dist.id, SourceType.module.id, LangType.js.id, ['dist'], ['src', ...theUsualSuspects], false),
+    makeConfig(ResourceLocation.dist.id, SourceType.commonjs.id, LangType.js.id, ['dist'], ['src', ...theUsualSuspects], false),
+];
+
+
+if (LOG_VERBOSE) {
+    console.log(`[${logTag}] Configurations:`);
+    for (const cfg of configs) {
+        console.log(`\n[${logTag}] --------------------------------`);
+
+        const logInfo = {
+            ...cfg,
+
+            languageOptions: {
+                ...cfg.languageOptions,
+                parser: '{ ommitted for brevity }',
+                globals: '{ ommitted for brevity }',
+            },
+
+            plugins: cfg.plugins ? `[${Object.keys(cfg.plugins).join(', ')}]` : cfg.plugins,
+            files: Array.isArray(cfg.files) ? `[${cfg.files.join(', ')}]` : cfg.files,
+            ignores: Array.isArray(cfg.ignores) ? `[${cfg.ignores.join(', ')}]` : cfg.ignores,
+            rules: '{ ommitted for brevity }',
+        };
+
+        console.log(inspect(logInfo, { depth: 3 }));
+    }
+}
+
+
+// EXPORTS
+
+
+export default configs;
+
+
+if (process.env.DEBUG && DEBUG_THIS) console.log(`╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ ${logTag} ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯`);
+if (process.env.DEBUG && DEBUG_THIS && DEBUG_PAUSE) debugger;
